7ff4af5b9c4170bb699fba69116b988b15025036
Brighton Tandabantu <brighton.tandabantu@fullstack.co.za>
2023-02-27 19:11:49 +0200

    recommended refactoring

--- VideoEncoder.cs
+++ VideoEncoder.cs
@@ -10,8 +10,7 @@
         //2. Define an event based on that delegate
         //3. Raise the event
 
-        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs eventArgs);
-        public event VideoEncodedEventHandler VideoEncoded;
+        public event EventHandler<VideoEventArgs> VideoEncoded;
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding videooo...");
